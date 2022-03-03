fac182140335c0151f620eb2c04498fd6e3a1dc5
Peter <prigali>
2022-03-03 00:36:29 -0600

    Age Represented

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,15 +78,20 @@ elif select == 'What Age is Represented?':
         df = pd.pivot_table(temp_df, values='Total', index='Year', columns='Age').sum()
     st.bar_chart(df)
 
+    ages = st.multiselect('Ages', age_lst)
     if year != 'All':
         ratio_df = pd.pivot_table(age_df, values='Total', index='Year', columns='Age').loc[year]
+        st.dataframe(ratio_df[ages])
+        st.dataframe(ratio_df.loc['TOTAL'])
     else:
         ratio_df = pd.pivot_table(age_df, values='Total', index='Year', columns='Age')
-    age_lst = ['18 THRU 19', '_20_', '_21_', '22 THRU 24', '25 THRU 34', '35 THRU 44', '45 THRU 54', '55 THRU 59',
-               '60 THRU 61', '62 THRU 64', '65 THRU 74', 'ABOVE 75']
-    ages = st.multiselect('Ages', age_lst)
-    st.dataframe(ratio_df[ages])
-    st.dataframe(ratio_df['TOTAL'])
+        st.dataframe(ratio_df[ages])
+        st.dataframe(ratio_df['TOTAL'])
+
+
+
+
+
     # st.dataframe(dfn)
     # col1 = st.columns(1)
     # col1.metric(year, str(sum([df[i] for i in ages]) / df['TOTAL']))

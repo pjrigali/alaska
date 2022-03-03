1778d6d307b4c9d32c102f28021560efe671ab38
Peter <prigali>
2022-03-03 00:47:17 -0600

    Age Represented

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,9 +81,9 @@ elif select == 'What Age is Represented?':
     if year != 'All':
         ratio_df = pd.pivot_table(age_df, values='Total', index='Year', columns='Age').loc[year]
         st.dataframe(ratio_df[ages])
-
+        val = sum(ratio_df[ages].tolist()) / ratio_df['TOTAL']
         col1 = st.columns(1)
-        col1.metric(year, str(ratio_df[ages].sum() / ratio_df['TOTAL'].values))
+        col1.metric(year, str(val))
     else:
         ratio_df = pd.pivot_table(age_df, values='Total', index='Year', columns='Age')
         st.dataframe(ratio_df[ages])

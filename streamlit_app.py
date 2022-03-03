e307ee982e6936ebd5e361422fb965f40909b7d3
Peter <prigali>
2022-03-02 23:49:25 -0600

    Age Represented

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,6 +92,7 @@ elif select == 'What Age is Represented?':
         #         age_ind.append(i)
         # temp_df = age_df.iloc[age_ind]
         df = pd.pivot_table(age_df, values='Total', index='Year', columns='Age')
+        st.dataframe(df)
         df = df[df['Age'] == ages]
         st.line_chart(df)
 
